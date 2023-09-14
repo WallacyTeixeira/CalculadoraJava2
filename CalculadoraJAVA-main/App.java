91586c2384f023a2dd66ee676535433a965549dd
Wallacy <Wallacy.teixeira@outlook.com>
2023-09-14 19:15:41 -0300

    15/14/13

--- CalculadoraJAVA-main/App.java
+++ CalculadoraJAVA-main/App.java
@@ -2,11 +2,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-       
-        new Calculadora();
         new CalculadoraCustoViagem();
-        new CalculadoraJavaIMC();
-
     }
 }
 
--- CalculadoraJAVA-main/CalculadoraCustoViagem.java
+++ CalculadoraJAVA-main/CalculadoraCustoViagem.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class CalculadoraCustoViagem extends JPanel {
     public CalculadoraCustoViagem() {
-
+        
         // Criação de um JFrame (janela principal) e configuração de propriedades
         JFrame frame = new JFrame("Calculadora de Viagem");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define a ação ao fechar a janela
@@ -14,47 +14,39 @@ public class CalculadoraCustoViagem extends JPanel {
 
         // Criação dos painéis para os botões numéricos
         JPanel painel1 = new JPanel();
-        JPanel painel2 = new JPanel();
-        JPanel painel3 = new JPanel();
-        
+
         // Adiciona os painéis ao JFrame em posições específicas
-        frame.add(painel1, BorderLayout.WEST); // Adiciona o painel1 à esquerda do JFrame
-        frame.add(painel2, BorderLayout.SOUTH); // Adiciona o painel2 na parte inferior do JFrame
-        frame.add(painel3, BorderLayout.EAST); // Adiciona o painel3 à direita do JFrame
+        frame.add(painel1, BorderLayout.SOUTH); // Adiciona o painel1 à esquerda do JFrame
         
         // Configuração dos layouts dos painéis de botões numéricos
-        painel1.setLayout(new BoxLayout(painel1, BoxLayout.Y_AXIS)); // Define layout vertical para o painel1
-        painel2.setLayout(new BoxLayout(painel2, BoxLayout.X_AXIS)); // Define layout horizontal para o painel2
-        painel3.setLayout(new BoxLayout(painel3, BoxLayout.Y_AXIS)); // Define layout vertical para o painel3
+        painel1.setLayout(new BoxLayout(painel1, BoxLayout.X_AXIS)); // Define layout vertical para o painel1
         
         // Adiciona botões numéricos aos painéis de botões numéricos
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             painel1.add(new JButton("" + (i + 1))); // Adiciona botões de 1 a 10 ao painel1
-            painel2.add(new JButton("" + (i + 5))); // Adiciona botões de 6 a 15 ao painel2
-            painel3.add(new JButton("" + (i + 10))); // Adiciona botões de 11 a 20 ao painel3
         }
 
         // Criação dos painéis de entrada para dados
         JPanel inputPanelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT)); // Criação de um painel com layout à direita
         JLabel distanciaLabel = new JLabel("Distância (km):"); // Criação de um rótulo para a distância
-        JTextField distanciaTextField = new JTextField(10); // Criação de um campo de texto para a distância
-        distanciaTextField.setFont(new Font("Arial", 0, 20)); // Define a fonte do campo de texto
+        JTextField distanciaTextField = new JTextField(5); // Criação de um campo de texto para a distância
+        distanciaTextField.setFont(new Font("Arial", 0, 25)); // Define a fonte do campo de texto
 
         inputPanelRight.add(distanciaLabel); // Adiciona o rótulo ao painel
         inputPanelRight.add(distanciaTextField); // Adiciona o campo de texto ao painel
 
         JPanel inputPanelLeft = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Criação de um painel com layout à esquerda
         JLabel precoLabel = new JLabel("Preço por litro:"); // Criação de um rótulo para o preço por litro
-        JTextField precoTextField = new JTextField(10); // Criação de um campo de texto para o preço por litro
-        precoTextField.setFont(new Font("Arial", 0, 20)); // Define a fonte do campo de texto
+        JTextField precoTextField = new JTextField(5); // Criação de um campo de texto para o preço por litro
+        precoTextField.setFont(new Font("Arial", 0, 25)); // Define a fonte do campo de texto
 
         inputPanelLeft.add(precoLabel); // Adiciona o rótulo ao painel
         inputPanelLeft.add(precoTextField); // Adiciona o campo de texto ao painel
 
         JPanel inputPanelCenter = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Criação de um painel com layout centralizado
         JLabel consumoLabel = new JLabel("Consumo por litro:"); // Criação de um rótulo para o consumo por litro
-        JTextField consumoTextField = new JTextField(10); // Criação de um campo de texto para o consumo por litro
-        consumoTextField.setFont(new Font("Arial", 0, 20)); // Define a fonte do campo de texto
+        JTextField consumoTextField = new JTextField(5); // Criação de um campo de texto para o consumo por litro
+        consumoTextField.setFont(new Font("Arial", 0, 25)); // Define a fonte do campo de texto
 
         inputPanelCenter.add(consumoLabel); // Adiciona o rótulo ao painel
         inputPanelCenter.add(consumoTextField); // Adiciona o campo de texto ao painel
@@ -63,14 +55,14 @@ public class CalculadoraCustoViagem extends JPanel {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Criação de um painel com layout centralizado
         JButton calcularButton = new JButton("Calcular"); // Criação de um botão com o texto "Calcular"
         JLabel resultadoLabel = new JLabel("Custo total:"); // Criação de um rótulo para o resultado
-        resultadoLabel.setFont(new Font("Arial", 0, 35)); // Define a fonte do rótulo
+        resultadoLabel.setFont(new Font("Arial", 0, 25)); // Define a fonte do rótulo
 
         buttonPanel.add(calcularButton); // Adiciona o botão "Calcular" ao painel
         buttonPanel.add(resultadoLabel); // Adiciona o rótulo "Custo total" ao painel
 
         // Criação do rótulo para mostrar o resultado do cálculo
         JLabel resultadoValor = new JLabel(""); // Criação de um rótulo vazio
-        resultadoValor.setFont(new Font("Arial", 0, 48)); // Define a fonte do rótulo
+        resultadoValor.setFont(new Font("Arial", 0, 30)); // Define a fonte do rótulo
 
         buttonPanel.add(resultadoValor); // Adiciona o rótulo de resultado ao painel
 
